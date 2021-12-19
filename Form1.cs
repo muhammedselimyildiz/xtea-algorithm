0846e6ae02c889d6d2d064ee419427bffb8f08d0
M. Selim Yildiz <muhammed.yildiz11@ogr.sakarya.edu.tr>
2021-12-19 03:48:04 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace xtea_algorithm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Xtea a = new Xtea();
+            string str = textBox1.Text;
+            string ky = textBox2.Text;
+            textBox5.Text= a.Encrypt(str, ky);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Xtea a = new Xtea();
+            string str = textBox4.Text;
+            string ky = textBox3.Text;
+            textBox6.Text = a.Decrypt(str, ky);
+        }
+    }
+}
---
+++ Xtea.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xtea_algorithm
+{
+    public class Xtea
+    {
+
+
+        private static uint[] Longer(byte[] s)
+        {
+            var long1 = new UInt32[(int)Math.Ceiling(((decimal)s.Length / 4))];
+
+            for (int i = 0; i < long1.Length; i++)
+            {
+                long1[i] = ((s[i * 4])) +
+                       ((i * 4 + 1) >= s.Length ? (UInt32)0 << 8 : ((UInt32)s[i * 4 + 1] << 8)) +
+                       ((i * 4 + 2) >= s.Length ? (UInt32)0 << 16 : ((UInt32)s[i * 4 + 2] << 16)) +
+                       ((i * 4 + 3) >= s.Length ? (UInt32)0 << 24 : ((UInt32)s[i * 4 + 3] << 24));
+            }
+
+            return long1;
+        }
+
+        private static byte[] ToBytes(uint[] l)
+        {
+            byte[] b = new byte[l.Length * 4];
+
+            for (int i = 0; i < l.Length; i++)
+            {
+                b[(i * 4)] = (byte)(l[i] & 0xFF);
+                b[(i * 4) + 1] = (byte)(l[i] >> (8 & 0xFF));
+                b[(i * 4) + 2] = (byte)(l[i] >> (16 & 0xFF));
+                b[(i * 4) + 3] = (byte)(l[i] >> (24 & 0xFF));
+            }
+            return b;
+        }
+
+        public string Encrypt(string text, string key)
+        {
+            if (text.Length == 0)
+                return "";
+
+            if (key.Length == 0)
+                throw new ArgumentException("Key is short");
+
+            while (key.Length < 16)
+                key += key;
+
+            var y = Longer((new UTF8Encoding()).GetBytes(text));
+            if (y.Length == 1)
+                y[0] = 0;
+
+            var k = Longer((new UTF8Encoding()).GetBytes(key.Substring(0,16)));
+
+            uint a = (uint)y.Length, b = y[a - 1], c = y[0], delta = 0x9e3779b9, d, f = (uint)(6 + (52 / a)), sum = f * delta, i = 0;
+
+            while (f-- > 0)
+            {
+                sum += delta;
+                d = sum >> 2 & 3;
+
+               for(i= 0; i < (a - 1); i++)
+                {
+                    c = y[(i + 1)];
+                    b=y[i] +=(b >> 5 ^ c << 2) + (c >> 3 ^ b << 4) ^ (sum ^ c) + (k[i & 3 ^ d] ^ b);
+                }
+
+                c = y[0];
+                b= y[a - 1] += (b >> 5 ^ c << 2) + (c >> 3 ^ b << 4) ^ (sum ^ c) + (k[i & 3 ^ d] ^ b);
+
+                
+            }
+
+            return Convert.ToBase64String(ToBytes(y));
+        }
+
+        public string Decrypt(string chipertext, string key)
+        {
+            if (chipertext.Length == 0)
+                return "";
+
+            var y = Longer(Convert.FromBase64String(chipertext));
+            var k = Longer((new UTF8Encoding()).GetBytes(key.Substring(0, 16)));
+
+            uint a = (uint)y.Length, b = y[a - 1], c = y[0], delta = 0x9e3779b9, d, f = (uint)(6 + (52 / a)), sum = 0, i = 0;
+
+            while (sum != 0)
+            {
+                d = sum >> 2 & 3;
+                for (i = (a - 1); i > 0; i--)
+                {
+                    c = y[(i - 1)];
+                    b = y[i] -= (b >> 5 ^ c << 2) + (c >> 3 ^ b << 4) ^ (sum ^ c) + (k[i & 3 ^ d] ^ b);
+                }
+
+                c = y[a - 1];
+                b= y[0] -= (b >> 5 ^ c << 2) + (c >> 3 ^ b << 4) ^ (sum ^ c) + (k[i & 3 ^ d] ^ b);
+
+                sum -= delta;
+            }
+
+            var plaintext = (new UTF8Encoding()).GetString(ToBytes(y));
+
+            return plaintext;
+        }
+
+        
+    }
+}
